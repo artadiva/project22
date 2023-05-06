878ecc712fd053578a9b64e683f88bc847663c27
artadiva30@gmail.com <artadiva30@gmail.com>
2023-05-07 00:18:49 +0800

    login register berhasil

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/home', function () {
     return view('dashboard');
 })->name('dashboard');
 Route::get('/page2', function () {
     return view('page2');
 })->name('page2');
+
+Auth::routes();
+
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
